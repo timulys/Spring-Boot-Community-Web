66f32ff2d4bba896900ca6d08a20d98a99cacc20
DESKTOP-035QAMS\jhchoi <timulys@gmail.com>
2018-12-18 18:50:50 +0900

    navigation 추가(resource파일 github 다운받아야 함)

--- src/main/java/me/timulys/SpringBootCommunityWeb/SpringBootCommunityWebApplication.java
+++ src/main/java/me/timulys/SpringBootCommunityWeb/SpringBootCommunityWebApplication.java
@@ -21,28 +21,25 @@ public class SpringBootCommunityWebApplication {
 	}
 
 	@Bean
-	public CommandLineRunner runner(UserRepository userRepository,
-									BoardRepository boardRepository) throws Exception {
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
 		return (args) -> {
 			User user = userRepository.save(User.builder()
 					.name("havi")
 					.password("test")
-					.email("test@gmail.com")
+					.email("havi@gmail.com")
 					.createdDate(LocalDateTime.now())
-					.build()
-			);
+					.build());
 
-			IntStream.rangeClosed(1, 200).forEach(index -> {
-				boardRepository.save(Board.builder()
-						.title("게시글" + index)
-						.subTitle("순서" + index)
-						.content("콘텐츠")
-						.boardType(BoardType.free)
-						.createdDate(LocalDateTime.now())
-						.createdDate(LocalDateTime.now())
-						.user(user).build()
-				);
-			});
+			IntStream.rangeClosed(1, 200).forEach(index ->
+					boardRepository.save(Board.builder()
+							.title("게시글"+index)
+							.subTitle("순서"+index)
+							.content("컨텐츠")
+							.boardType(BoardType.free)
+							.createdDate(LocalDateTime.now())
+							.updatedDate(LocalDateTime.now())
+							.user(user).build())
+			);
 		};
 	}
 }
